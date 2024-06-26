eee6b4d0646365198a60b815c63e8559c1f5c538
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-06-26 19:27:08 +0200

    make faq section responsive

--- src/components/layout/main/AppFaqs.vue
+++ src/components/layout/main/AppFaqs.vue
@@ -21,17 +21,32 @@ export default {
     <section id="about">
 
         <div class="container">
-            <div class="img-container">
-                <img src="../../../assets/imgs/single-image2.png" class="w-100" alt="">
-            </div>
-            <h1> {{ store.sections[4].title }}</h1>
-            <div class="row flex-column">
-                <div class="col" v-for="(FAQ, index) in store.sections[4].FAQs" :key="index">
-                    <CollapseFaqs :question="FAQ.question" :answer="FAQ.answer" :index="'faq-' + index"></CollapseFaqs>
+
+            <div class="row">
+
+                <div class="col col-md-6">
+                    <div class="img-container">
+                        <img src="../../../assets/imgs/single-image2.png" class="w-100" alt="">
+                    </div>
                 </div>
+
+                <div class="col col-md-6">
+                    <div class="row flex-column">
+                        <h1> {{ store.sections[4].title }}</h1>
+
+                        <div class="col" v-for="(FAQ, index) in store.sections[4].FAQs" :key="index">
+                            <CollapseFaqs :question="FAQ.question" :answer="FAQ.answer" :index="'faq-' + index">
+                            </CollapseFaqs>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <div class="row flex-column">
-                <div class="col" v-for="card in store.sections[4].Cards">
+
+
+
+            <div class="row flex-column flex-md-row">
+                <div class="col col-md-4" v-for="card in store.sections[4].Cards">
                     <CardFAQ :title="card.title" :text="card.text" :icon="card.icon"></CardFAQ>
                 </div>
             </div>
